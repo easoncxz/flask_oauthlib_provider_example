736647f8657a54a6b589a35b81782ac0c23b7743
Eason Chen <easoncxz@gmail.com>
2015-08-31 23:38:50 +1200

    Added __repr__ in models.py for easy debugging.

--- models.py
+++ models.py
@@ -7,6 +7,10 @@ class User:
         self.username = username
         self.password = password
 
+    def __repr__(self):
+        return "<User(username={}, password={})>".format(
+                self.username, self.password)
+
 class Client:
 
     def __init__(self, user, redirect_uris):
@@ -15,6 +19,13 @@ class Client:
         self.client_key = gen_salt(30)
         self.client_secret = gen_salt(40)
         self.default_realms = ['r']
+
+    def __repr__(self):
+        return ("<Client(user={}, redirect_uris={}, "
+                "client_key={}, client_secret={}, "
+                "default_realms={})>").format(
+                    self.user, self.redirect_uris,
+                    self.client_key, self.client_secret, self.default_realms)
 
     @property
     def default_redirect_uri(self):
@@ -31,6 +42,13 @@ class RequestToken:
         self.secret = secret
         self.verifier = gen_salt(40)
 
+    def __repr__(self):
+        return ("<RequestToken(client={}, user={}, "
+                "redirect_uri={}, realms={}, "
+                "token={}, secret={}, verifier={})>").format(
+                    self.client, self.user, self.redirect_uri, self.realms,
+                    self.token, self.secret, self.verifier)
+
 class Nonce:
 
     def __init__(
@@ -46,6 +64,12 @@ class Nonce:
         self.request_token = request_token
         self.access_token = access_token
 
+    def __repr__(self):
+        return ("<Nonce(client_key={}, timestamp={}, nonce={}, "
+                "request_token={}, access_token={})>").format(
+                    self.client_key, self.timestamp, self.nonce,
+                    self.request_token, self.access_token)
+
 class AccessToken:
 
     def __init__(self, client, user):
@@ -54,3 +78,9 @@ class AccessToken:
         self.realms = ['r']
         self.token = gen_salt(30)
         self.secret = gen_salt(40)
+
+    def __repr__(self):
+        return ("<AccessToken(client={}, user={}, realms={}, "
+                "token={}, secret={})>").format(
+                    self.client, self.user, self.realms,
+                    self.token, self.secret)
